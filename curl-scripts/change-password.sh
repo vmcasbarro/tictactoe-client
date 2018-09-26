ae6302d8c639012f1983948a1f58272791927a3a
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-09-26 15:31:35 -0400

    change password update

--- curl-scripts/change-password.sh
+++ curl-scripts/change-password.sh
@@ -1,4 +1,4 @@
-curl "https://tic-tac-toe-wdi.herokuapp.com//change-password" \
+curl "https://tic-tac-toe-wdi.herokuapp.com/change-password" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
