b6aac3c9c604975080e2abe3592dd566086c3655
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-09-26 22:27:16 -0400

    add board event listeners for each square, alternate x and o, add character to space on board
    - event listener in app.js
    - event in events.js
    - game logic in game.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const authEvents = require('./auth/events.js')
+const gameEvents = require('./game/events.js')
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
@@ -9,9 +10,20 @@ const authEvents = require('./auth/events.js')
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // AUTH
   $('#signup-form').on('submit', authEvents.onSignUp)
   $('#signin-form').on('submit', authEvents.onSignIn)
   $('#change-password-form').on('submit', authEvents.onChangePassword)
   $('#signout-button').on('click', authEvents.onSignOut)
+
+  //GAME
+  $('#0').on('click', gameEvents.onSelect)
+  $('#1').on('click', gameEvents.onSelect)
+  $('#2').on('click', gameEvents.onSelect)
+  $('#3').on('click', gameEvents.onSelect)
+  $('#4').on('click', gameEvents.onSelect)
+  $('#5').on('click', gameEvents.onSelect)
+  $('#6').on('click', gameEvents.onSelect)
+  $('#7').on('click', gameEvents.onSelect)
+  $('#8').on('click', gameEvents.onSelect)
 })
---
+++ assets/scripts/game/events.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const getFormFields = require('./../../../lib/get-form-fields.js')
+const api = require('./api.js')
+const ui = require('./ui.js')
+const game = require('../../../lib/game.js')
+
+const onSelect = function (event) {
+  //console.log(event.target.id)
+  const index = event.target.id
+  game.addLetter(index)
+}
+
+module.exports = {
+  onSelect
+
+}
